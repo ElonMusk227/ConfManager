b8663772165670196d66990088d14904bec51ba0
Assogba Isaac <assogbaisaac321@gmail.com>
2024-09-04 18:02:30 +0100

    Adding creator page and modifing the header and the footer page

---
+++ creators.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <title>
+    Creators
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons -->
+  <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="./assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
+  <link id="pagestyle" href="./assets/css/style.css" rel="stylesheet" />
+</head>
+<body class="bg-gray-200 justify-content-center text-center">
+    <h1 class="font-weight-bold">Creators</h1>
+    <div class="container py-5" style="margin-left:300px">
+        <div class="row ">
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/drake.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Assogba Charles Isaac</h5>
+                    <h6 class="card-subtitle">Isaac's Code</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/marie.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Adamou Zakari Leila</h5>
+                    <h6 class="card-subtitle">Leila's Cake</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/drake.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Mahamadou Hamissou</h5>
+                    <h6 class="card-subtitle">Hamissou Telecom</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/marie.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Adamou Incha Fourera</h5>
+                    <h6 class="card-subtitle">Short Girl</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/marie.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Bakoye Hadjara</h5>
+                    <h6 class="card-subtitle">Dan Wadata</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+
+            <div class="card mb-3 mx-3 justify-content-center text-center" style="width: 18rem;  padding:0px">
+                <img src="./assets/img/drake.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Assogba Charles Isaac</h5>
+                    <h6 class="card-subtitle">Isaac's Code</h6>
+                    <a href="#" class=" mt-3 btn btn-primary">Go somewhere</a>
+                </div>  
+            </div>
+        </div>
+    </div>
+<?php 
+    include './elements/footer.php';
+?>
--- elements/footer.php
+++ elements/footer.php
@@ -4,7 +4,8 @@
             <div class="copyright text-center text-muted">
                 © <script>
                     document.write(new Date().getFullYear())
-                </script>, made with <i class="fa fa-heart text-danger"></i> by
+                </script>, made  by
+                <a href="./creators.php" class="font-weight-bold">Team Winners</a> and code and powered by
                 <a href="https://www.linkedin.com/assogbaisaac" class="font-weight-bold" target="_blank">Assogba Isaac</a>
                 for a better web.
             </div>
